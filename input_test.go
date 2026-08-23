f3b0ef9180b82a66ac05cd1dc58576fe497a1b2c
Kasi Viswanath Aurangabad <kasi.aurangabadx@gmail.com>
2026-08-23 11:36:56 +0530

    feat: carry native call origin in VM input

---
+++ input_test.go
@@ -0,0 +1,20 @@
+package vmcommon
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+// NON_NORMATIVE_DRWA_PROTOTYPE
+// DO_NOT_EXPOSE_AS_PUBLIC_WIRE_FORMAT
+// REPLACED_BY_PART_B
+
+func TestNativeCallOriginPrototypeValuesAndDefault(t *testing.T) {
+	t.Parallel()
+
+	input := VMInput{}
+	require.Equal(t, NativeCallOriginUnknown, input.NativeCallOrigin)
+	require.Equal(t, NativeCallOrigin(0), NativeCallOriginUnknown)
+	require.Equal(t, NativeCallOrigin(1), NativeCallOriginOriginalUserTransaction)
+}
